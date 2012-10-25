70913dc819b389b1b2b16cc0a1b35a66a2382235
Hoàng Minh Thắng <p@banphim.net>
2012-10-25 18:00:49 +0700

    refactor count and contains? in boot.cljs

--- resources/private/boot.cljs
+++ resources/private/boot.cljs
@@ -1,11 +1,11 @@
 (defmacro apply [fun & args] `(.apply ~fun ~fun ~@args))
-(defmacro count [x] `(inline ~(str (clojurejs.js/emit-str x) ".length")))
+(defmacro count [x] `(get ~x :length))
 (defmacro not [expr] `(! ~expr))
 (defn empty? [s] (or (undefined? s) (nil? s) (= 0 (count s))))
 (defn not-empty? [s] (not (empty? s)))
 
 (defmacro contains? [m k]
-  `(inline ~(str (clojurejs.js/emit-str k) " in " (clojurejs.js/emit-str m))))
+  `(in ~k ~m))
 
 (defmacro when [pred & body] `(if ~pred (do ~@body)))
 (defmacro when-not [pred & body] `(if (not ~pred) (do ~@body)))
