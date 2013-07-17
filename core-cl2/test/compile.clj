8dc3620ff257b903d9e8f113cb73616320612689
Hoàng Minh Thắng <p@banphim.net>
2013-07-17 13:15:42 +0700

    fix test/compile: files passed to tojs should be in abs paths

--- core-cl2/test/compile.clj
+++ core-cl2/test/compile.clj
@@ -1,19 +1,18 @@
 (ns compile
-  (:require [chlorine.js :refer [tojs' *temp-sym-count* *macros*]]
+  (:require [chlorine.js :refer [tojs]]
             [chlorine.util :refer [replace-map]]))
 
 (defn -main [& files]
-  (doseq [file files]
+  (doseq [file files
+          :let [file (-> file
+                         clojure.java.io/file
+                         .getAbsolutePath)]]
     (let [output
           (-> file
-              clojure.java.io/file
-              .getAbsolutePath
               (replace-map
                (array-map #".cl2$" ".js",
                           "/core-cl2/test/" "/test_runners/"))
               clojure.java.io/file)]
       (when-not (.isDirectory (.getParentFile output))
         (.mkdirs (.getParentFile output)))
-      (spit output (binding [*temp-sym-count* (ref 999)
-                             *macros*         (ref {})]
-                     (tojs' file))))))
+      (spit output (tojs file)))))
